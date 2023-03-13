c6de9ced92dc7fe69b7b5e8d7b15514a858982f9
woodbyte <pedro.s.madeira@gmail.com>
2023-03-13 21:59:03 +0000

    Add project files.

---
+++ SubModule.cs
@@ -0,0 +1,29 @@
+﻿using HarmonyLib;
+using TaleWorlds.Core;
+using TaleWorlds.MountAndBlade;
+using static TaleWorlds.MountAndBlade.Agent;
+
+namespace Bannerlord.DeadlySiegeEngines
+{
+    public class SubModule : MBSubModuleBase
+    {
+        protected override void OnSubModuleLoad()
+        {
+            Harmony harmony = new Harmony("bannerlord.deadlysiegeengines");
+            harmony.PatchAll();
+        }
+
+        [HarmonyPatch(typeof(Agent))]
+        [HarmonyPatch(nameof(Agent.Die))]
+        internal class Patch01
+        {
+            static void Prefix(ref Blow b, KillInfo overrideKillInfo = KillInfo.Invalid)
+            {
+                if (b.IsMissile && b.DamageType == DamageTypes.Blunt && b.WeaponRecord.WeaponClass == WeaponClass.Boulder)
+                {
+                    b.DamageType = DamageTypes.Pierce;
+                }
+            }
+        }
+    }
+}
